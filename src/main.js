7da5bf95d4534b3d13251c7b35a39c57efcd5c52
Minh Tuan VO <vominhtuan164@gmail.com>
2020-11-09 11:25:42 +0100

    add Header & BaseCard components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+const app = createApp(App);
 
-createApp(App).mount('#app');
+app.component('base-card', BaseCard);
+
+app.mount('#app');
